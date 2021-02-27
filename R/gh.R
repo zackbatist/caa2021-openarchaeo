f1ab344890beb0ba7710dad3f4532a3ae8d8b7f9
Joe Roe <joe@joeroe.io>
2021-02-27 12:01:47 +0100

    Replace ght() with specific functions for each GitHub API endpoint
    Scrapping the generalised approach; the responses are too variable.

--- R/gh.R
+++ R/gh.R
@@ -1,53 +1,178 @@
 # gh.R
-# Vectorised wrapper functions for the GitHub API
+# Functions for accessing the GitHub API
 
-#' Query the GitHub API (tabulated)
+#' GitHub repository queries
 #'
-#' [dplyr::mutate()]-friendly wrapper for [gh::gh()] that tabulates the response
-#' and always returns a vector that matches the number of queries.
-#' Vectorised over the named API parameters in `...`.
+#' Get the languages, contributors, issues, comments, or commits of a single
+#' repository from the GitHub API.
+#'
+#' @param repo Name of the repository, in the form `"owner/repository"`.
+#'
+#' @return
+#' A `tibble` containing the tabulated API responses, or `NA` if the response
+#' was empty or unsuccessful.
+#'
+#' @details
+#' Queries use [ghq()], a thin wrapper for [gh::gh()] that ensures the query
+#' always returns a value.
+#'
+#' @name gh_query
+#'
+#' @examples
+#' gh_lang("joeroe/era")
+#' gh_contrib("joeroe/era")
+#' gh_issue("joeroe/era")
+#' gh_comment("joeroe/era")
+#' gh_commit("joeroe/era")
+NULL
+
+#' @rdname gh_query
+#' @export
+gh_lang <- function(repo) {
+  data <- tibble::tibble(
+    repo = repo,
+    response = ghq("GET /repos/{repo}/languages", repo = repo)
+  )
+
+  if (not_all_na(data$response)) {
+    data <- dplyr::mutate(data, lang = names(response))
+    data <- tidyr::unnest(data, response)
+    data <- dplyr::select(data, repo, lang, bytes = response)
+    data <- dplyr::select(data, where(not_all_na))
+    data
+  }
+  else NA
+}
+
+#' @rdname gh_query
+#' @export
+gh_contrib <- function(repo) {
+  data <- tibble::tibble(
+    repo = repo,
+    response = ghq("GET /repos/{repo}/contributors", repo = repo)
+  )
+
+  if (not_all_na(data$response)) {
+    data <- tidyr::hoist(data, response,
+                         contributor = "login",
+                         contributions = "contributions")
+    data <- dplyr::select(data, -response)
+    data
+  }
+  else NA
+}
+
+#' @rdname gh_query
+#' @export
+gh_issue <- function(repo) {
+  data <- tibble::tibble(
+    repo = repo,
+    response = ghq("GET /repos/{repo}/issues", repo = repo)
+  )
+
+  if (not_all_na(data$response)) {
+    data <- tidyr::hoist(data, response,
+                         num = "number",
+                         "title",
+                         author = c("user", "login"),
+                         "labels",
+                         "state",
+                         "comments",
+                         "created_at",
+                         "updated_at",
+                         "closed_at",
+                         "body")
+    data <- dplyr::select(data, -response)
+    data <- dplyr::mutate(data,
+      created_at = lubridate::as_datetime(created_at),
+      updated_at = lubridate::as_datetime(updated_at),
+      closed_at = lubridate::as_datetime(closed_at),
+    )
+    data
+  }
+  else NA
+}
+
+#' @rdname gh_query
+#' @export
+gh_comment <- function(repo) {
+  data <- tibble::tibble(
+    repo = repo,
+    response = ghq("GET /repos/{repo}/issues/comments", repo = repo)
+  )
+
+  if (not_all_na(data$response)) {
+    data <- tidyr::hoist(data, response,
+                         issue_num = "issue_url",
+                         author = c("user", "login"),
+                         "created_at",
+                         "updated_at",
+                         "body")
+    data <- dplyr::select(data, -response)
+    data <- dplyr::mutate(data,
+                          issue_num = stringr::str_match(data$issue_num,
+                                                         "\\/(\\d*)$")[,2],
+                          created_at = lubridate::as_datetime(created_at),
+                          updated_at = lubridate::as_datetime(updated_at))
+    data
+  }
+  else NA
+}
+
+#' @rdname gh_query
+#' @export
+gh_commit <- function(repo) {
+  data <- tibble::tibble(
+    repo = repo,
+    response = ghq("GET /repos/{repo}/commits", repo = repo)
+  )
+
+  if (not_all_na(data$response)) {
+    data <- tidyr::hoist(data, response,
+                         "sha",
+                         author = c("author", "login"),
+                         datetime = c("commit", "author", "date"))
+    data <- dplyr::select(data, -response)
+    data <- dplyr::mutate(data,
+                          datetime = lubridate::as_datetime(datetime))
+    data
+  }
+  else NA
+}
+
+not_all_na <- function(x) any(!is.na(x))
+
+#' Query the GitHub API
+#'
+#' Thin wrapper for [gh::gh()] that returns an NA if the response is empty or
+#' if returns a 404 error (with a warning).
 #'
 #' @param endpoint GitHub API endpoint.
 #' @param ... Other arguments to [gh::gh()], including name-value pairs of any
 #'  API parameters in `endpoint`.
 #'
 #' @details
-#' Empty responses return an `NA` row, with a warning if the request produced
-#' a 404 error. Queries where any value of `...` is `NA` will also return an
-#' `NA` row.
-#'
-#' @return
-#' If each response returns a single row, a `tibble` with one row per query
-#' (producing a packed column if used in `mutate()`). Otherwise, a list of
-#' `tibbles` for each query (producing a nested column if used in `mutate()`).
+#' Queries where any value of `...` is `NA` will also return an `NA` row.
 #'
 #' @export
 #'
 #' @examples
-#' ght("GET /repos/{owner}/{repo}/languages", owner = c("joeroe", "joeroe"), repo = c("era", "c14"))
-ght <- function(endpoint, ...) {
-  responses <- purrr::pmap(data.frame(...), function(endpoint, ...) {
-    if (any(is.na(list(...)))) data.frame(`NA` = NA)
-    else {
-      res <- tryCatch(
-        gh(endpoint, ...),
-        http_error_404 = gh_warn_404
-      )
-      if (length(res) > 0) dplyr::bind_rows(res)
-      else data.frame(`NA` = NA)
-    }
-  }, endpoint = endpoint)
-
-  if (all(sapply(responses, nrow) == 1)) {
-    responses <- dplyr::bind_rows(responses)
-    if ("NA" %in% names(df)) responses <- dplyr::select(responses, -`NA`)
+#' ghq("GET /repos/{repo}/languages", repo = "joeroe/c14")
+#' ghq("GET /repos/{repo}/languages", repo = "nonexistent/repo")
+ghq <- function(endpoint, ...) {
+  if (any(is.na(list(...)))) NA
+  else {
+    res <- tryCatch(
+      gh(endpoint, ..., .limit = Inf),
+      http_error_404 = gh_warn_404
+    )
+    if (length(res) > 0) res
+    else NA
   }
-
-  return(responses)
 }
 
 gh_warn_404 <- function(c) {
   w <- stringr::str_extract(as.character(c), "URL not found:.*$")
   rlang::warn(paste0("GitHub API ", w), class = "http_error_404")
-  return(list())
+  return(NA)
 }
